8e2982abc55c648666137c8e6d31454532ba696b
Fernanda Domenicali <43413565+nandadomenicali@users.noreply.github.com>
2022-05-25 20:37:23 -0300

    Implementing model class

--- src/users/users-model.js
+++ src/users/users-model.js
@@ -1,3 +1,6 @@
+const userDTO = require('./userDTO');
+const { InvalidArgumentError } = require('../erros');
+const validations = require('../validations');
 
 class User {
   constructor(user) {
@@ -6,9 +9,51 @@ class User {
     this.email = user.email;
     this.password = user.password;
 
-    this.valida();
+    this.check();
+  } 
+
+  async add() {
+    if (await User.findByEmail(this.email)) { 
+      throw new InvalidArgumentError('User already exists!');
+    }
+ 
+    return userDTO.add(this);
   }
 
+  check() {
+    validations.notNull(this.name, 'name');
+    validations.notNull(this.email, 'email');
+    validations.notNull(this.password, 'password');
+    validations.minSize(this.password, 'password', 8);  
+    validations.maxSize(this.password, 'password', 64);
+  }
+ 
+  
+  async delete() {
+    return userDTO.delete(this); 
+  } 
+    
+  static async findById(id) {
+    const user = await userDTO.findById(id);
+    if (!user) {
+      return null;
+    }
+     
+    return new User(user); 
+  }
+   
+  static async findByEmail(email) {
+    const user = await userDTO.findByEmail(email);
+    if (!user) {
+      return null;
+    }
+    
+    return new User(user);
+  }
+ 
+  static list() {
+    return userDTO.list();
+  }
 }
-
-module.exports = User; 
+ 
+module.exports = User;  
